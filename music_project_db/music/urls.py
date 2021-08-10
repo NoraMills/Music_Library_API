c2ede1f1861c25a68c1064d5d558016aee6ceb9f
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-08-10 00:25:33 -0500

    created views

--- music_project_db/music/urls.py
+++ music_project_db/music/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
 from . import views
+ #allows get and post requests
+from rest_framework import routers
 
+
+router = routers.DefaultRouter()
+router.register('music', views.SongView)
 
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
--- music_project_db/music/views.py
+++ music_project_db/music/views.py
@@ -1,8 +1,8 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from .models import Song
 from rest_framework import viewsets
+from .models import Song
 from .serializers import SongSerializer
-from music_project_db.music import serializers
 
 
 class SongView(viewsets.ModelViewSet):
