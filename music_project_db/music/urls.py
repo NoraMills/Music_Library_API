c7608ae0d663fb1ae641960becc7aef3481d95f3
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-08-10 00:00:32 -0500

    removed code that will not be used

--- music_project_db/music/urls.py
+++ music_project_db/music/urls.py
@@ -1,10 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
-
 urlpatterns = [
-    path("music/", views.SongList.as_view()),
-    path('music/<int:pk>/', views.SongDetail.as_view()),
-    path('music/like/<int:pk>/', views.LikeSong.as_view()),
-    ]
+    
+]
--- music_project_db/music/views.py
+++ music_project_db/music/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from .models import Song
 from .serializers import SongSerializer
 from rest_framework.views import APIView
@@ -7,9 +8,8 @@ from django.http import Http404
 
 
 # Create your views here.
-
-
 class SongList(APIView):
+
     def get(self, request):
         song = Song.objects.all()
         serializer = SongSerializer(song, many=True)
@@ -22,49 +22,4 @@ class SongList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-class SongDetail(APIView):
-
-    def get_object(self, pk):
-        try:
-            return Song.objects.get(id=pk)
-        except Song.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        song = self.get_object(pk)
-        serializer = SongSerializer(song)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def put(self, request, pk):
-        song = self.get_object(pk)
-        serializer = SongSerializer(song, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        song = self.get_object(pk)
-        serializer = SongSerializer(song)
-        song.delete()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-class LikeSong(APIView):
-
-    def get_object(self, pk):
-        try:
-            return Song.objects.get(id=pk)
-        except Song.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        song = self.get_object(pk)
-        song.likes +=1
-        song.save()
-        serializer = SongSerializer(song)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def ranking(self, request, pk):
-        song = self.get_object(pk)
-        pass
+ 
